0637f340cf158b7e35cc540a08a8703e0cb69d7e
Student <student@imac-astrum-138.local>
2022-09-16 15:01:28 +0500

    Not completed

--- beckend/urls.py
+++ beckend/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", home),
+    path('index/', index, name='Bosh sahifa'),
     path('contact/', contact, name='contact'),
+    path('kiyimlar/', kiyimlar, name='kiyimlar'),
 ]
--- beckend/views.py
+++ beckend/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 def home(request):
     return render(request,'index.html')
 
+def index(request):
+    return render(request,'index.html')
 
 def contact(request):
     return render(request,'contact.html')
+
+def kiyimlar(request):
+    return render(request,'messages.html')
